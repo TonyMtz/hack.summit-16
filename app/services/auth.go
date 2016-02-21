69e6f55882948e567ebed04aa8a3c249bd01aef4
Julio Gutierrez <bubuntux@gmail.com>
2016-02-21 00:24:36 -0600

    getting token from wunderlist

--- app/services/auth.go
+++ app/services/auth.go
@@ -1,6 +1,9 @@
 package services
 
-import "github.com/revel/revel"
+import (
+	"github.com/revel/revel"
+	//"log"
+)
 
 type Provider interface {
 	RedirectUrl() string
@@ -12,8 +15,10 @@ var (
 )
 
 func init() {
+	//log.Print("trello" +  revel.Config.StringDefault("trello.key", "default"))
 	providers = make(map[string]Provider)
 	providers["trello"] = NewTrello("afb6671d5446eb923f98a0111aa8227d", "4508a1f0f51d4e77ec3f32f87bfdd3b63048fffa659040952f012a9e02986ad5") //TODO
+	providers["wunderlist"] = NewWunderlist("3db038e285c0dfc22875", "ab16496c9830bcf716fd7a6770dc745e7a9d70a8829c0801a6d27258b35e") //TODO
 }
 
 func Auth(provider string) string {
---
+++ app/services/wunderlist.go
@@ -0,0 +1,51 @@
+package services
+
+import (
+	"log"
+	"fmt"
+	"github.com/revel/revel"
+	"golang.org/x/oauth2"
+)
+
+func init() {
+	oauth2.RegisterBrokenAuthHeaderProvider("https://www.wunderlist.com/oauth/")
+}
+
+type Wunderlist struct {
+	config *oauth2.Config
+}
+
+func NewWunderlist(key string, secret string) Wunderlist {
+	w := Wunderlist{config:&oauth2.Config{
+		ClientID:    key,
+		ClientSecret: secret,
+		Scopes:       []string{},
+		Endpoint:     oauth2.Endpoint{
+			AuthURL:"https://www.wunderlist.com/oauth/authorize",
+			TokenURL:"https://www.wunderlist.com/oauth/access_token",
+		},
+	}}
+	return w
+}
+
+func (w Wunderlist) RedirectUrl() string {
+	callbackUrl := fmt.Sprintf("http://%s:%v/wunderlist/callback", revel.HttpAddr, revel.HttpPort) //TODO common code
+	w.config.RedirectURL = callbackUrl
+	return w.config.AuthCodeURL("RANDOM")
+}
+
+func (w Wunderlist) Callback(params revel.Params) string {
+	values := params.Query
+	verificationCode := values.Get("code")
+
+	token, err := w.config.Exchange(oauth2.NoContext, verificationCode)
+	if err != nil {
+		log.Fatal(err) //TODO throw?
+		return "Error!"
+	}
+	return token.AccessToken
+}
+
+/*func (c Wunderlist) connected() *models.User {
+	return c.RenderArgs["user"].(*models.User)
+}*/
